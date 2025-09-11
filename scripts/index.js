5806680f22914cb0d3cc7b4e98fc255942a9c798
Kyle <Kylethompsonchauder@gmail.com>
2025-09-11 15:26:55 -0600

    Rendered out the names list no condition and when they are younger than 40 for the first 2 lists

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 
 // sample data - expanded Star Wars characters with varied ages
 const users = [
-  { id: 1, name: "Luke Skywalker" age: 23 },
+  { id: 1, name: "Luke Skywalker", age: 23 },
   { id: 2, name: "Darth Vader", age: 45 },
   { id: 3, name: "Princess Leia", age: 23 },
   { id: 4, name: "Obi-Wan Kenobi", age: 57 },
@@ -15,11 +15,29 @@ const users = [
 ];
 
 // broken test data for exercise 6
-
+const namesList = document.getElementById("names-list");
+const youngCharactersList = document.getElementById("young-characters-list");
+const renderingCharactersList = document.getElementById("function-list");
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
+users.forEach((user) => {
+  const listItem = document.createElement("li");
+
+  console.log(user.name);
+
+  listItem.textContent = user.name;
+  namesList.appendChild(listItem);
+});
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
+users.forEach((user) => {
+  if (user.age < 40) {
+    const listItem = document.createElement("li");
+    console.log(user.name);
 
+    listItem.textContent = user.name;
+    youngCharactersList.appendChild(listItem);
+  }
+});
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
